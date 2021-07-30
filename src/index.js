e049de81295ab2d624cf431f98688b5ce07b4266
PetiurAliona <alena.petyur@gmail.com>
2021-07-30 16:14:17 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import '@pnotify/core/dist/BrightTheme.css';
-import '@pnotify/core/dist/PNotify.css';
-import { error } from '@pnotify/core';
-const debounce = require('lodash.debounce');
+// import '@pnotify/core/dist/BrightTheme.css';
+// import '@pnotify/core/dist/PNotify.css';
+// import { error } from '@pnotify/core';
+// const debounce = require('lodash.debounce');
