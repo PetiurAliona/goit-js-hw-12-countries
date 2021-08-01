758539f8ffc520647de4a8de99058da2e495ce67
PetiurAliona <alena.petyur@gmail.com>
2021-08-01 17:31:10 +0300

    +

--- src/index.js
+++ src/index.js
@@ -4,3 +4,8 @@ import "@pnotify/core/dist/PNotify.css";
 import "@pnotify/core/dist/BrightTheme.css";
 import { error } from "@pnotify/core";
 import { fetchCountries } from './js/fetchCountries';
+import countryCard from './tpl/countryCardTpl'
+import countryList from './tpl/countryListTpl'
+
+document.querySelector('.country_input').addEventListener('input', debounce(fetchCountries, 500));
+const countryCardNode = document.querySelector('.country_card');
