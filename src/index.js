6e55a694860d39759f39a26fd5fb2b23e47b7d3b
PetiurAliona <alena.petyur@gmail.com>
2021-08-01 22:26:49 +0300

    +index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 import debounce from 'lodash.debounce';
 
-import "@pnotify/core/dist/PNotify.css";
-import "@pnotify/core/dist/BrightTheme.css";
-import { error } from "@pnotify/core";
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import { error } from '@pnotify/core';
 import { fetchCountries } from './js/fetchCountries';
-import countryCard from './tpl/countryCardTpl'
-import countryList from './tpl/countryListTpl'
 
-document.querySelector('.country_input').addEventListener('input', debounce(fetchCountries, 500));
+import countryCard from './tpl/countryCardTpl';
+import countryList from './tpl/countryListTpl';
+
+document.querySelector('.country_input').addEventListener('input', debounce(onInputAction, 500));
+
 const countryCardNode = document.querySelector('.country_card');
+
+function onInputAction(e) {
+  fetchCountries(e.target.value).then(findCountry);
+}
+
+function findCountry(countries) {
+  if (countries.length === 1) {
+    countryCardNode.innerHTML = countryCard(countries[0]);
+  } else if (countries.length <= 10 && countries.length > 1) {
+    const obj = {};
+    obj.country = countries.map(country => country.name);
+    countryCardNode.innerHTML = countryList(obj);
+  } else if (countries.length > 10) {
+    error({ text: 'Too many matches found. Please enter a more specific query', delay: 1500 });
+  } else if (countries.status === 404) {
+    error({ text: 'Country is not found', delay: 1500 });
+  }
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,26 +1,7 @@
-
-import { error } from "@pnotify/core";
-import countryCard from '../tpl/countryCardTpl'
-import countryList from '../tpl/countryListTpl'
-
-const countryCardNode = document.querySelector('.country_card');
+import { error } from '@pnotify/core';
 
 export function fetchCountries(searchQuery) {
-  return fetch('https://restcountries.eu/rest/v2/name/' + searchQuery.target.value)
+  return fetch('https://restcountries.eu/rest/v2/name/' + searchQuery)
     .then(response => response.json())
-     .then(countries => {
-      if (countries.length === 1) {
-        countryCardNode.innerHTML = countryCard(countries[0]);
-      } else if (countries.length <= 10 && countries.length > 1) {
-        const obj = {};
-        obj.country = countries.map(country => country.name);
-        countryCardNode.innerHTML = countryList(obj);
-      } else if (countries.length > 10){
-        countryCardNode.innerHTML = '';
-                error({ text: 'Too many matches found. Please enter a more specific query' });
-              }
-    })
+    .catch(e => error({ text: `${e}`, delay: 1500 }));
 }
-
-
-// fetchCountries(name)
